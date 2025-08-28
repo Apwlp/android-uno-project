c7555eaa56e3753e3fbeb5427aa7b26899effdb7
Apwlp <sebastian.salazarcm@gmail.com>
2025-08-28 10:12:56 -0500

    Creacion segunda pantalla y degradado de la misma

---
+++ app/src/main/java/com/example/android_uno_project/game.java
@@ -0,0 +1,12 @@
+package com.example.android_uno_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class game extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
